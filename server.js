fcf5021626d0d43426cabd9a5ea4fb65dd2d1895
Edin Mavric <edinmavric10@gmail.com>
2023-05-08 14:14:58 +0200

    sending more data to a server

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.json({ "users": ["playerOne", "playerTwo", "playerThree", "playerFour"] });
+  const data = {
+    "players": ["playerOne", "playerTwo", "playerThree", "playerFour"],
+    "points": ["16.3", "26.1", "7.7", "3.1"],
+  };
+  res.json(data);
 });
 
 app.listen(5000, () => {
